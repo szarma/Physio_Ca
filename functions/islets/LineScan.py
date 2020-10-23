e4638c5450136a1a4ea54d7a0444d339f2957e89
szarma <2512087+szarma@users.noreply.github.com>
2020-10-23 14:45:16 +0200

    added LineScan functions

---
+++ functions/islets/LineScan.py
@@ -0,0 +1,214 @@
+import numpy as np
+import pandas as pd
+from itertools import product
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import networkx as nx
+from .numeric import rebin
+from .utils import multi_map
+import plotly.graph_objects as go
+
+
+class LineScan:
+    def __init__(self, data,
+                 metadata=None,
+                 freq=None,
+                 verbose=False,
+                 name=None,
+                 detrend=True
+                ):
+        self.data = data.astype("float32")
+        if metadata is None:
+            if freq is None:
+                raise ValueError("If you do not pass metadata, you need to at least provide frequency.")
+            else:
+                self.metadata = pd.Series({
+                    "SizeX":data.shape[0],
+                    "pxSize":data.shape[0],
+                    "pxUnit":"px",
+                    "Frequency":freq
+                })
+        else:
+            assert metadata["SizeX"]== data.shape[0]
+            self.metadata = metadata
+        self.Freq = self.metadata.Frequency
+        self.time = np.arange(0,data.shape[1])/self.Freq
+        self.name = name
+        if detrend:
+            self.detrend()
+        
+    def plot(self,
+        tmax=None,
+        timeScales = [None,3],
+        distances = [5,10,20,50,100],
+        times = [.5,1,2,5,10,30],
+        save=False,
+        Npoints = 1000,
+        verbose=False,
+        ):
+        if tmax is None:
+            tmax = self.time[-1]
+        physicalSize = self.metadata[["SizeX","pxSize"]].prod()
+        distance = distances[np.searchsorted(distances, physicalSize*.15)-1]
+        Tind = times[np.searchsorted(times, tmax*.2)-1]
+        txtOffset = physicalSize-1.2*distance
+        nr = len(timeScales)
+        fig, axs = plt.subplots(nr,1,
+                                figsize=(4,3*nr),
+                                sharey=True, 
+                                sharex=True
+                               )
+        fig.patch.set_facecolor('k')
+        plt.subplots_adjust(wspace=.02, hspace=.02)
+        nRebin = int(np.round(len(self.time)/Npoints))
+#         print (nRebin)
+        if nRebin>1:
+            if verbose: print ("rebinning by", nRebin)
+            newdata = rebin(self.data, nRebin, axis=1)
+            ls = LineScan(newdata, freq=self.Freq/nRebin)
+        else:
+            ls = self
+        for it in range(len(timeScales)):
+            ts = timeScales[it]
+            if ts is None:
+                txt = "raw"
+                x = ls.data
+            else:
+                txt = "detrended"
+                try:
+                    s,x,z = ls.fast_filter_traces(ts,write=False)
+                except:
+                    continue
+            ax = axs[it]
+            ax.set_yticks([])
+            ax.set_xticks([])
+            ax.imshow(x,extent=(0, x.shape[1]/ls.Freq,0,physicalSize,),
+                      aspect="auto",
+                      cmap="hot",
+                      vmin=0
+                     )
+            ax.set_ylabel(txt,color="lightgrey")
+            if it==len(timeScales)-1:
+                ax.plot([tmax*.03]*2,[txtOffset,txtOffset+distance],"lightgrey")
+                ax.text(tmax*.03,txtOffset+distance/2,"  "+str(distance)+"µm", va="center", color="w")
+                ax.plot([tmax*.03, tmax*.03+Tind],[.03*physicalSize]*2,"lightgrey")
+                ax.text(tmax*.03+Tind/2,.03*physicalSize,str(Tind)+"s\n", ha="center", va="center", color="w")
+            if it==0 and self.name is not None:
+                ax.set_title(self.name,color='w')
+        ax.set_xlim(0,tmax)
+        if save:
+            fig.tight_layout()
+            fig.savefig(save, dpi=300, facecolor='k')
+            plt.close(fig)
+        else:
+            fig.show()
+
+    def detrend(self, fast=True, n=None, processes=1, func=None, points=100):
+        from islets.numeric import mydebleach
+        from islets.utils import multi_map
+        if n is None:
+            n = self.data.shape[0]
+        self.detrended = np.zeros_like(self.data)
+        if fast:
+            trend = np.median(self.data[:n],1)
+        else:
+            global iterf
+            def iterf(xi):
+                return mydebleach(xi, func=func, points=points)
+            trend = multi_map(iterf, self.data[:n], processes = processes)
+            if processes==1:
+                trend = list(trend)
+        for i in range(n):
+            self.detrended[i] = self.data[i] - trend[i]
+    
+    def examine(self, debug=False):
+        from .linescanner import examine
+        return examine(self, debug=debug)
+    
+    def fast_filter_traces(self,
+                           ironTimeScale,
+                           z_sp=0,
+                           order=5,
+                           Npoints=None,
+                           write=True,
+                           verbose=False,
+                           dilation=False
+                          ):
+        from .numeric import fast_filter
+        if hasattr(self,"gain"):
+            gain = self.gain
+        else:
+            if z_sp==0:
+                gain = 1
+            else:
+                raise ValueError("z_sp allowed to be non zero only if gain is provided. If the data represent raw photon counts, put gain to 1.")
+            
+        x_slow, x_fast, zscore = fast_filter(
+            self.data,
+            ironTimeScale= ironTimeScale,
+            gain=gain,
+            freq=self.Freq,
+            z_sp=z_sp,
+            order=5,
+            verbose=verbose,
+            npass=2,
+            nreflect="auto",
+            dilation=dilation
+              )
+        if write:
+            setattr(self, "slower_%g"%ironTimeScale, x_slow)
+            setattr(self, "faster_%g"%ironTimeScale, x_fast)
+            setattr(self, "zScore_%g"%ironTimeScale, zscore)
+        else:
+            return x_slow, x_fast, zscore
+        
+    def infer_gain(self, plot=False, verbose=False):
+        ts = 100/self.Freq
+        absSlow, absFast, _ = self.fast_filter_traces(ts,write=False, normalize=False,z_sp=0)
+        di = 100
+        slow_est, fast_vars = [],[]
+        for i in range(absFast.shape[0]):
+            for j in range(di, absFast.shape[1]-di, absFast.shape[1]//30):
+                slow_est  += [absSlow[i,j]]
+                fast_vars += [absFast[i,j-di:j+di].var()]
+        fast_vars = np.array(fast_vars)
+        slow_est = np.array(slow_est)
+        slow_est[slow_est<=0] = slow_est[slow_est>0].min()
+        logbs = np.log(np.logspace(np.log10(np.percentile(slow_est,2)),np.log10(min(np.percentile(slow_est,98), self.data.max()*.4))))
+        d = np.digitize(np.log(slow_est), logbs)
+        x = np.array([slow_est[d==i].mean() for i in np.unique(d)])
+        y = np.array([np.median(fast_vars[d==i]) for i in np.unique(d)])
+        gain = np.mean(y/x)
+#         gain = np.exp(np.mean(np.log(y)-np.log(x)))
+        
+        if plot:
+            ax = plt.subplot(111)
+            ax.hexbin(slow_est, fast_vars, bins="log",
+                      xscale="log",
+                      yscale="log",
+                      cmap="Greys",
+                      mincnt=1
+                     )
+            c = ax.plot(x,y,"o",mfc="none")[0].get_color()
+            ax.plot(x,x*gain,c=c)
+            
+        if verbose: print ("initial estimate of the gain is", gain)
+        
+        for _ in range(5):    
+            fast_vars[fast_vars>10*gain*slow_est] = np.nan
+            if np.isnan(fast_vars).any():
+                y = np.array([np.nanmedian(fast_vars[d==i]) for i in np.unique(d)])
+                newgain = np.nanmean(y/x)
+                if verbose: print ("revised estimate of the gain", gain)
+                if plot:
+                    c = ax.plot(x,y,"o",mfc="none")[0].get_color()
+                    ax.plot(x,x*gain,c=c)
+                if np.abs(newgain/gain-1)<.03:
+                    if verbose:
+                        print("gain converged.")
+                    break
+                gain = newgain
+        
+        self.gain = gain
+
+    
---
+++ functions/islets/linescanner.py
@@ -0,0 +1,254 @@
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+import plotly.graph_objects as go
+import json
+from jupyter_plotly_dash import JupyterDash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+from dash import no_update
+from sys import exc_info
+from .numeric import robust_max
+from .utils import getFigure
+from .numeric import rebin
+
+
+def take_part(ls, key, extent):    
+    if extent is None:
+        extent = (None,)*4
+    x0,x1,t0,t1 = extent
+    if x0 is None: x0 = 0
+    if x1 is None: x1 = ls.data.shape[0]
+    if t0 is None: 
+        it0 = 0
+    else:
+        it0 = np.searchsorted(ls.time, t0)
+    if t1 is None: 
+        it1 = ls.data.shape[1]
+    else:
+        it1 = np.searchsorted(ls.time, t1)
+    x0 = int(x0)
+    x1 = min(int(x1)+1, ls.data.shape[0])
+    if x1-x0<=1: x1=x0+1
+    try:
+        getattr(ls,key)
+    except:
+        timescale = float(key.split("_")[-1])
+        ls.fast_filter_traces(timescale,z_sp=0)
+    return x0,x1,it0,it1
+
+def plot_heatmap(ls, key="zScore_2", extent=None, NTimepoints=300):
+    from islets.numeric import robust_max
+    x0,x1,it0,it1 = take_part(ls,key,extent)
+    nt = it1-it0
+    nr = int(nt/NTimepoints)
+    Z = getattr(ls,key)[x0:x1, it0:it1]
+    tr = ls.time[it0:it1]
+    if nr>1:
+        z = rebin(Z,nr,1)
+        tr = rebin(tr, nr)
+    else:
+        z=Z
+    fig = go.Figure(
+        data=go.Heatmap(z=z,
+                        showscale=False, 
+                        zmin=-robust_max(z,min(100,int(z.size*.1))) if ("zScore" in key or key=="detrended") else .5,
+                        zmax=robust_max(z,min(100,int(z.size*.1))),
+                        colorscale="RdBu_r" if ("zScore" in key or key=="detrended") else "hot",
+                        x=tr,
+                        y=np.arange(x0,x1), 
+                        hoverinfo="skip",
+                        
+                       ),
+        layout={
+            "dragmode":'select',
+            "width":350,
+            "height":380,
+            "margin":{"t":20,**dict(zip("lbr",[3]*3))},
+            "xaxis":{"title":"time [s]"},
+            "yaxis":{"title":"line index"}
+        }
+    )
+    fig.add_traces(go.Scatter(x=[ls.time[[it0,it1-1]].mean()],y=[(x0+x1)/2],opacity=0, hoverinfo='skip'))
+    
+#     fig.add_annotation(
+#                 x=0.05,
+#                 y=0.95,
+#                 text=f"rebinned by {nr}",
+#                 xref="paper",
+#                 yref="paper",
+#                 showarrow=False,
+#                 font_size=20
+#     )
+    return fig
+
+
+def plot_trace(ls, key="data", extent=None, nr=1):
+    x0,x1,it0,it1 = take_part(ls,key,extent)
+    Z = getattr(ls,key)[x0:x1, it0:it1]
+    z = Z.mean(0)
+    tr = ls.time[it0:it1]
+    if nr>1:
+        z = rebin(z,nr)
+        tr = rebin(tr, nr)
+    
+    fig = go.Figure(
+        data=go.Scatter(x=tr, y=z,line_width=1,line_color="darkgrey"),
+        layout={
+            "width":500,
+            "height":390,
+            "margin":{"t":30,**dict(zip("lbr",[3]*3))},
+            "xaxis":{"title":"time [s]"}
+        }
+                    )
+    return fig
+
+
+# overviewGraph = 
+# zoomGraph     = dcc.Graph(id='zoomed-fig',  figure=getFigure(),)
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll',
+        'overflowY': 'scroll',
+        'height':"300px",
+        'width':"400px",
+    },
+    "flex":{
+        "padding":"10px","max-width":"550px","max-height":"800px","border":'thin lightgrey solid',
+    }
+}
+
+def examine(linescan, debug=False):
+    marks = np.array([i*10**j for i in [1,2,5] for j in range(5)])
+    marks.sort()
+    marks = marks[marks<linescan.Freq]
+    app = JupyterDash(__name__, width=1500, height=1200)
+
+    app.layout = html.Div([
+        html.H1("LineScanner"),
+        html.Div([
+            html.Div([
+                html.H3(graphID.capitalize()+" figure"),
+                dcc.RadioItems(id=f"{graphID}-radio",
+                               options=[{"label":"raw data","value":"data"}, {"label":"filtered","value":"zScore_2"}, {"label":"detrended","value":"detrended"}], 
+                               value= "detrended", 
+    #                            value= {"zoom":None,"overview":"zScore_2"}[graphID]
+                              ),
+                dcc.Graph(id=f'{graphID}-fig',figure=getFigure(),)
+            ]+[
+                html.Div(
+                    ["rebin by",
+                     html.Div(dcc.Slider(id="rebin-slider", 
+                                         min=0, max=len(marks)-1,
+                                         updatemode="drag",
+                                         value=np.searchsorted(marks,linescan.Freq/10)-1
+    #                                      marks = {i:{"label":str(marks[i])} for i in range(len(marks))}
+                                        ), style={"width":"300px"}),
+                     html.Div(id="slider-out",children=""),
+    #                  dcc.Input(id="rebin-value",type=int)
+                    ],style={"display":"flex", "flex-wrap":"wrap","align-items":"bottom","flex-shrink":3}),
+              ]*int(graphID=="trace"),style=styles['flex']) for graphID in ["overview", "zoom","trace"]
+        ],style={"display":"flex", "flex-wrap":"wrap","align-items":"top","flex-shrink":3}),
+        html.Div([
+            html.Pre(id='relayout-data', style={"display":"block" if debug else "none"}),
+            html.Pre(id="output",        style={"display":"block" if debug else "none"}),
+            html.Pre(id="output2",       style={"display":"block" if debug else "none"}),
+        ],style={"display":"flex", "flex-wrap":"wrap","align-items":"top","flex-shrink":3}),
+        ])
+
+    #### callbacks
+    @app.callback(
+        Output('slider-out', 'children'),
+        [Input("rebin-slider","value")]
+    )
+    def show_rebin(value):
+        return str(marks[value])
+
+    @app.callback(
+        Output('overview-fig', 'figure'),
+        [Input("overview-radio","value")]
+    )
+    def show_overview(value):
+        return plot_heatmap(linescan, value )
+
+
+    @app.callback(
+        Output('relayout-data', 'children'),
+        [Input('overview-fig', 'selectedData')])
+    def display_data(data_):
+        return json.dumps(data_["range"], indent=2)
+
+
+    @app.callback(
+        [Output('zoom-fig', 'figure'),
+        Output('output', 'children')],
+        [Input('overview-fig', 'selectedData'), 
+         Input("zoom-radio","value")])
+    def replot(data_,radioValue):
+        try:
+            t0,t1 = data_["range"]["x"]
+            x0,x1 = data_["range"]["y"]
+            return plot_heatmap(linescan, radioValue, (x0,x1,t0,t1), ), "ok"
+        except:
+            fig = getFigure(350,380,"white")
+            text="Choose a box <br> select tool <br> in the overview plot <br> to select area <br> to zoom in"
+            fig.add_annotation(
+                        x=0.5,
+                        y=0.5,
+                        text=text,
+                        xref="paper",
+                        yref="paper",
+                        showarrow=False,
+                        font_size=20
+            )
+            return fig, str(exc_info())
+
+    @app.callback(
+        [Output('trace-fig', 'figure'),
+        Output('output2', 'children')],
+        [Input('overview-fig', 'selectedData'), 
+         Input('zoom-fig', 'selectedData'), 
+         Input("trace-radio","value"),
+         Input("rebin-slider","value")
+        ])
+    def plot_trace_callback(data_ov, data_zoom, radioValue, i_rebin):
+        try:
+            if data_zoom is None:
+                if data_ov is None:
+                    data_ = None
+                else:
+                    data_ = data_ov
+            else:
+                data_ = data_zoom
+            if data_ is None:
+                extent = None
+            else:
+                t0,t1 = data_["range"]["x"]
+                x0,x1 = data_["range"]["y"]    
+                extent = (x0,x1,t0,t1)
+            nr = int(marks[i_rebin])
+            try:
+                fig = plot_trace(linescan, radioValue, extent, nr=nr)
+            except:
+                fig = getFigure()
+    #         text = f"rebinned by {nr}"
+    #         if extent is not None:
+    #             text += "<br>extent=(%.1f,%.1f,%.1f,%.1f)"%extent
+    #         fig.add_annotation(
+    #                     x=0.05,
+    #                     y=0.95,
+    #                     text=text,
+    #                     xref="paper",
+    #                     yref="paper",
+    #                     showarrow=False,
+    #                     font_size=20
+    #         )
+            return fig,"ok"
+        except:
+            return getFigure(), str(exc_info())
+
+
+    return app
