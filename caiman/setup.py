b39128a548d9c6398d415b4dae0c30bec565d822
Srdjan <srdjan.sarikas@gmail.com>
2021-02-22 14:05:28 +0100

    dummy caiman added

---
+++ caiman/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='caiman',
+    version="0",
+    description='Empty caiman',
+    #packages=find_packages(),
+    #long_description=long_description,
+    #install_requires=requirements,
+    author='CTN'
+)
