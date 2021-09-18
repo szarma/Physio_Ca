98c0294397559297a3a64b5a74edf7bc902844e2
Johannes Pfabe <johannes.pfabe@meduniwien.ac.at>
2021-09-18 17:20:38 +0200

    Updated docker to pin versions. Still issues with connectivity of ContainDS-dashboards.

--- docker/jupyterhub/jupyterhub_config.py
+++ docker/jupyterhub/jupyterhub_config.py
@@ -8,7 +8,7 @@ c = get_config()
 
 c.JupyterHub.log_level = 'DEBUG'
 c.JupyterHub.admin_access = True
-c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
 c.JupyterHub.allow_named_servers = True
 
 c.JupyterHub.template_paths = CDS_TEMPLATE_PATHS
@@ -20,11 +20,11 @@ c.CDSDashboardsConfig.presentation_types = ['plotlydash']
 c.Authenticator.admin_users = {'admin'}
 c.LocalAuthenticator.create_system_users = True
 
-#c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+# c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.spawner_class = 'cdsdashboards.hubextension.spawners.variabledocker.VariableDockerSpawner'
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+c.DockerSpawner.volumes = {'jupyterhub-user-{username}': notebook_dir}
 c.DockerSpawner.name_template = "{prefix}-{username}-{servername}"
 c.DockerSpawner.debug = True
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
@@ -42,4 +42,3 @@ c.JupyterHub.services = [
         ],
     }
 ]
-
