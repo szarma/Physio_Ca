3b9abae0ce6925b48e6873da83e9d3f1bb62aafc
Johannes Pfabe <johannes.pfabe@meduniwien.ac.at>
2021-09-27 16:29:26 +0200

    Added functionality to recognize series.

--- app/DatabaseManager.py
+++ app/DatabaseManager.py
@@ -7,16 +7,22 @@ import pandas as pd
 from pathlib import Path
 import re
 import sqlite3
-from typing import List, NamedTuple, Optional, Tuple
+from typing import List, NamedTuple, Optional, Tuple, Union, Pattern
 from sys import stdout
 
 SUPPORTED_FORMATS = ['.nd2', '.lif']
 
 
 class DatabaseManager:
-    logger: logging.Logger
+    _series_regex: str
+    _protocol_regex: str
+    _pkl_regex: str
+    _compiled_ser_regex: Pattern[str]
+    _compiled_pro_regex: Pattern[str]
+    _compiled_pkl_regex: Pattern[str]
+    _logger: logging.Logger
     connection: sqlite3.Connection
-    cursor: sqlite3.Cursor
+    _cursor: sqlite3.Cursor
 
     def __init__(self, database_path: Path, verbose: bool):
         """
@@ -28,38 +34,46 @@ class DatabaseManager:
 
         # Initialize logging functions for output
         log_level = logging.DEBUG if verbose else logging.INFO
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(log_level)
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(log_level)
         handler = logging.StreamHandler(stdout)
         handler.setLevel(log_level)
-        self.logger.addHandler(handler)
+        self._logger.addHandler(handler)
 
         # Initialize connection to database
         if not database_path.is_file():
-            self.logger.error(f'Could not find file "{database_path.as_posix()}". New database will be created!')
+            self._logger.error(f'Could not find file "{database_path.as_posix()}". New database will be created!')
         try:
             self.connection = sqlite3.connect(database_path)
-            self.cursor = self.connection.cursor()
+            self._cursor = self.connection.cursor()
         except sqlite3.DatabaseError as e:
-            self.logger.error(f'"Unable to open {database_path.as_posix()}"!')
-            self.logger.error(f'Error: {e}')
+            self._logger.error(f'"Unable to open {database_path.as_posix()}"!')
+            self._logger.error(f'Error: {e}')
             raise IOError
 
         # Check if tables already exist and create them if they are not there.
-        self.logger.info('Checking if tables exist (create if not existing)...')
-        self.cursor.execute('''create table if not exists recording
-        (id int primary key autoincrement, path text not null , metadata int not null)''')
-        self.cursor.execute('''create table if not exists metadata
-        (id int primary key autoincrement, date date, frequency real, pixel_size real, size_x int, size_y int,
-        size_z int, size_t int)''')
-        self.cursor.execute('''create table if not exists experiment
-        (id int primary key autoincrement, pickles text, series text, path_to_movie text, path_to_pickles text,
-        path_to_additional_info text, recording int, duration real)''')
+        self._logger.info('Checking if tables exist (create if not existing)...')
+        self._cursor.execute('''create table if not exists recording
+        (id integer primary key autoincrement, path text not null , metadata integer not null)''')
+        self._cursor.execute('''create table if not exists metadata
+        (id integer primary key autoincrement, date date, frequency real, pixel_size real, size_x integer,
+        size_y integer, size_z int, size_t int)''')
+        self._cursor.execute('''create table if not exists experiment
+        (id integer primary key autoincrement, pickles text, series text, path_to_movie text, path_to_pickles text,
+        path_to_additional_info text, recording integer, duration real)''')
         self.connection.commit()
+
+        # Create regex entries and compile them to boost performance:
+        self._series_regex = r'\w+\d+-?\d+'
+        self._protocol_regex = r'.*protocol[.]txt'
+        self._pkl_regex = r'(?P<pickle_param>\d(?:\.\d)?)_rois[.]pkl'
+        self._compiled_ser_regex = re.compile(self._series_regex)
+        self._compiled_pro_regex = re.compile(self._protocol_regex)
+        self._compiled_pkl_regex = re.compile(self._pkl_regex)
 
     def __del__(self):
         self.connection.commit()
-        self.cursor.close()
+        self._cursor.close()
         self.connection.close()
 
     def refresh(self, root_dir: Path, wipe: bool = False):
@@ -73,9 +87,33 @@ class DatabaseManager:
 
         recordings = self.__get_recordings(root_dir=root_dir)
         self.__set_recordings(recording_list=recordings, wipe=wipe)
+        self.__set_experiments(recording_list=recordings, wipe=wipe)
         raise NotImplementedError
 
-    def __get_experiments(self):
+    def __get_experiments(self, recording_path: Path) -> List[Tuple[str, Path]]:
+        experiments: List[Tuple[str, Path]] = []
+
+        analysis_directory = recording_path.parent.joinpath(recording_path.name + '_analysis/')
+        if analysis_directory.is_dir():
+            self._logger.debug(f'Analysis directory: {analysis_directory.as_posix()}')
+            existing_series = []
+            for child in analysis_directory.iterdir():
+                if child.is_dir():
+                    if self._compiled_ser_regex.match(child.name) is not None:
+                        self._logger.debug(f'Matched {child.name} as a valid series of the analysis directory.')
+                        experiments.append((child.name, recording_path.joinpath(child.name)))
+                self._logger.debug(f'Series found: {existing_series.__str__()}')
+
+        return experiments
+
+    def __set_experiments(self, recording_list: List[Path], wipe: bool = False) -> None:
+        if wipe:
+            self._logger.info(f'Deleting existing data in order to refresh...')
+            self._cursor.execute('''delete from experiment''')
+        for recording in recording_list:
+            experiments = self.__get_experiments(recording)
+            for experiment_name, experiment_path in experiments:
+                pass
         raise NotImplementedError
 
     def __get_recordings(self, root_dir: Path) -> List[Path]:
@@ -83,22 +121,22 @@ class DatabaseManager:
         for path_object in root_dir.rglob('*'):
             try:
                 if path_object.suffix in SUPPORTED_FORMATS:
-                    self.logger.debug(f'Found file while indexing: "{path_object.as_posix()}"')
+                    self._logger.debug(f'Found file while indexing: "{path_object.as_posix()}"')
                     file_list.append(path_object)
             except PermissionError as e:
-                self.logger.error(f'Permission denied accessing file: "{e.filename}"')
-        self.logger.debug(f'Found {len(file_list)} recordings.')
+                self._logger.error(f'Permission denied accessing file: "{e.filename}"')
+        self._logger.debug(f'Found {len(file_list)} recordings.')
         return file_list
 
     def __set_recordings(self, recording_list: List[Path], wipe: bool = False) -> None:
         if wipe:
-            self.logger.info(f'Deleting existing data in order to refresh...')
-            self.cursor.execute('''delete from recording''')
+            self._logger.info(f'Deleting existing data in order to refresh...')
+            self._cursor.execute('''delete from recording''')
         for recording in recording_list:
-            metadata = self.__get_metadata(recording)
+            metadata = self.__get_metadata(recording_path=recording, identifier=None)
             metadata_entry = SqlMetadataEntry(
                 id=0,
-                date=datetime.datetime.now(),
+                date=metadata['date'],
                 frequency=.0,
                 pixel_size=.0,
                 size_x=0,
@@ -106,36 +144,47 @@ class DatabaseManager:
                 size_z=0,
                 size_t=0
             )
-            self.cursor.execute('''insert or replace into metadata(date) values (?)''',
-                                (metadata_entry.date,))
-            metadata_id = self.cursor.lastrowid
+            self._cursor.execute('''insert or replace into metadata(date) values (?)''',
+                                 (metadata_entry.date,))
+            metadata_id = self._cursor.lastrowid
 
             recording_entry = SqlRecordingEntry(
                 id=0,
                 path=recording,
                 metadata=metadata_id
             )
-            self.cursor.execute('''insert or replace into recording(path, metadata) values (?,?)''',
-                                (recording_entry.path.as_posix(), recording_entry.metadata))
+            self._cursor.execute('''insert or replace into recording(path, metadata) values (?,?)''',
+                                 (recording_entry.path.as_posix(), recording_entry.metadata))
         self.connection.commit()
-        raise NotImplementedError
 
-    def __get_metadata(self, recording_path: Path) -> pd.DataFrame:
-        metadata = pd.DataFrame(data={'date': datetime.datetime.now()})
+    def __get_metadata(self, recording_path: Path, identifier: Optional[Union[str, int]] = None) -> pd.DataFrame:
+        metadata = pd.DataFrame(data={'Name': None,
+                                      'SizeT': 0,
+                                      'SizeX': 0,
+                                      'SizeY': 0,
+                                      'SizeZ': 0,
+                                      'pxSize': 0.0,
+                                      'bit depth': None,
+                                      'Frequency': 0.0,
+                                      'Start time': datetime.datetime.now(),
+                                      'End time': datetime.datetime.now(),
+                                      'Duration': None,
+                                      'line scan': 0,
+                                      'date': datetime.datetime.now()})
         metadata_exists = recording_path.parent.joinpath(f'.{recording_path.name}.meta').is_file()
         if metadata_exists:
             meta_path = recording_path.parent.joinpath(f'.{recording_path.name}.meta')
             try:
-                self.logger.debug(f'Reading metadata of file "{meta_path.as_posix()}"')
+                self._logger.debug(f'Reading metadata of file "{meta_path.as_posix()}"')
                 metadata = pd.read_csv(meta_path, index_col=0)
             except IOError:
-                self.logger.error(f'Could not parse metadata of file "{meta_path.as_posix()}" !')
-                self.logger.debug(f'Setting metadata to None.')
+                self._logger.error(f'Could not parse metadata of file "{meta_path.as_posix()}" !')
+                self._logger.debug(f'Setting metadata to None.')
         else:
             try:
                 javabridge.start_vm(class_path=bioformats.JARS, run_headless=True)
             except RuntimeError:
-                self.logger.error(f'Error occurred while starting JVM! Please restart the kernel and try again.')
+                self._logger.error(f'Error occurred while starting JVM! Please restart the kernel and try again.')
                 exit()
             xml = bioformats.OMEXML(bioformats.get_omexml_metadata(recording_path))
             num_images = xml.get_image_count()
@@ -146,7 +195,7 @@ class DatabaseManager:
                 else:
                     metadata['date'] = xml.image().AcquisitionDate
             else:
-                self.logger.info(f'No recording data found in "{recording_path.as_posix()}". Skipped file.')
+                self._logger.info(f'No recording data found in "{recording_path.as_posix()}". Skipped file.')
 
         return metadata
 
@@ -160,7 +209,7 @@ class SqlRecordingEntry(NamedTuple):
 class SqlExperimentEntry(NamedTuple):
     id: int
     pickles: List[str]
-    series: Optional[Tuple[str, ...], List[str], str]
+    series: Union[List[str], str]
     movie_path: Path
     protocol_path: Path
     additional_info_path: Path
