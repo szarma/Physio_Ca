5a164c474bbba133d88bde71f48e13c5199ead03
Hannsen <1245029@students.vetmeduni.ac.at>
2021-10-24 10:46:54 +0200

    Some cosmetic changes to the serialization code.

--- islets/Regions.py
+++ islets/Regions.py
@@ -144,8 +144,8 @@ class Regions:
     def to_hdf5(self,
                 output_dir: Union[str, Path],
                 file_name: str = '',
-                movie = None,
-                col = None,
+                movie=None,
+                col: Optional[List[str]] = None,
                 formats: Optional[List] = None,
                 add_date: bool = True) -> None:
         """
@@ -235,8 +235,8 @@ class Regions:
                 output_dir: Union[str, Path],
                 file_name: str = '',
                 movie=None,
-                col: Union[list, None] = None,
-                formats: Union[list, None] = None,
+                col: Optional[list] = None,
+                formats: Optional[List] = None,
                 add_date: bool = True,
                 use_compression: bool = False) -> None:
         """
@@ -755,6 +755,7 @@ class Regions:
                 ax.set_xlim(xlim)
             except:
                 pass
+
         if scaleFontSize<=0: return None
         if hasattr(self, "metadata") and "pxSize" in self.metadata:
             lengths = [10,20,50,100,200,500]
