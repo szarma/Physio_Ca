b6fc8d92f8f44a96678f0514e0d60e48ec8d20e0
Hannsen <1245029@students.vetmeduni.ac.at>
2021-10-24 21:43:38 +0200

    Implemented Hdf5Serializer.py as inherited class in relation to IDataSerializer.py

---
+++ islets/serialization/IDataSerializer.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Union
+from ..Regions import Regions
+
+
+class IDataSerializer(ABC):
+    __file_path: Path
+
+    @abstractmethod
+    def __init__(self, file_path: Union[Path, str]):
+        pass
+
+    @property
+    @abstractmethod
+    def filepath(self) -> Path:
+        pass
+
+    @filepath.setter
+    @abstractmethod
+    def filepath(self, val: Path) -> None:
+        pass
+
+    @abstractmethod
+    def load(self, **kwargs) -> Regions:
+        pass
+
+    @abstractmethod
+    def save(self, **kwargs) -> None:
+        pass
