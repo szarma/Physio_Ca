27bc6c14b21090adf9a19badfd454b55cd9c279d
Srdjan <srdjan.sarikas@gmail.com>
2021-10-29 21:27:51 +0200

    motion_correct.py

--- scripts/motion_correct.py
+++ scripts/motion_correct.py
@@ -1,17 +1,18 @@
 #!/opt/tljh/user/envs/physio/bin/python
 
 import argparse
-            
+
 parser = argparse.ArgumentParser(description='Correct series for translational motion')
 parser.add_argument('--recording', '-rec', type=str, default="", help='path to the recording')
 parser.add_argument(   '--series', '-ser', type=str, default="", help='name of the series')
-parser.add_argument( '--restrict', type=str, default="", help='restrict analysis to the time interval (in seconds!), e.g. "0_100" will only process first 100 seconds of the movie')
+parser.add_argument( '--restrict', type=str, default="", help='restrict analysis to the time interval (in seconds!), e.g. "0_100" will only correct first 100 seconds of the movie')
 parser.add_argument(  '--verbose', const=True, default=False, action="store_const",
                     help='toggle verbose output')
-# parser.add_argument('--test', const=True, default=False, action="store_const",
-#                     help='toggle test mode on')
 parser.add_argument('--debug', const=True, default=False, action="store_const",
                     help='toggle debug mode')
+parser.add_argument('--correct-phase', const=True, default=False, action="store_const",
+                    help='whether to correct the phase also, default is False. In case phase is to be corrected, note that it will take first 1s of the recording as a template onto which to match. If that is not desirable, more input is needed.')
+
 
 args = parser.parse_args()
 
@@ -19,21 +20,18 @@ args = parser.parse_args()
 if args.debug:
     for k in args.__dict__.keys():
         print("%20s"%k, args.__dict__[k])
-    
+
 if args.verbose:
     print("importing modules...")
-    
+
 import numpy as np
 np.corrcoef([0,1,2],[0,2,1])
 
 import warnings
 import os
-import matplotlib.pyplot as plt
-import PIL
-
 from islets import cmovie
-from islets.numeric import rebin
 from islets.Recording import Recording, saveMovie
+from tifffile import memmap as tifmemmap
 
 import bioformats as bf
 bf.javabridge.start_vm(class_path=bf.JARS, max_heap_size="20G")
@@ -52,7 +50,7 @@ if isNikon:
     ser = os.path.split(recFile)[1].split(".nd2")[0]
 else:
     serToImport = ser
-    
+
 saveDir = os.path.join(rec.folder, rec.Experiment+"_analysis", ser)
 if restrict is not None:
     saveDir += f"_{restrict[0]}-{restrict[1]}s"
@@ -70,82 +68,62 @@ if args.verbose:
 
 rec.import_series(serToImport, restrict=restrict, )
 metadata = rec.Series[serToImport]['metadata']
-# metadata   = rec.Series[ser]["metadata"]
 freq = metadata.Frequency
-orig_movie = cmovie(rec.Series[ser]["data"][:-1], fr=freq)
-
-if args.debug:
-    assert False ########################## debug stop ###########
-    
-freqMC = 1
-n_rebin = int(np.round(freq/freqMC))
-if n_rebin<1:
-    n_rebin=1
-reb_movie = rebin(orig_movie,n_rebin,dtype='float32')
-reb_movie = cmovie(reb_movie, fr=freq/n_rebin)
-cell_half_width_in_px = int(np.ceil(6/metadata.pxSize))
-max_dev_rig = cell_half_width_in_px//2+1
-
-figs, axs = plt.subplots(2, 1, figsize=(8, 8), sharex=True, sharey=True)
-shifts = np.zeros((len(reb_movie), 2))
-template = reb_movie[len(reb_movie) // 2]
-while True:
-    tmp = reb_movie.motion_correct(
-        max_shift_w=max_dev_rig,
-        max_shift_h=max_dev_rig,
-        template=template
-    )
-    dshifts = np.array(tmp[1])
-    template = template
-    shifts += dshifts
-    maxshift = np.abs(dshifts).max()
-
-    axs[0].plot(np.arange(len(shifts)) / reb_movie.fr, shifts[:, 0])
-    axs[1].plot(np.arange(len(shifts)) / reb_movie.fr, shifts[:, 1])
-    if maxshift < max_dev_rig:
-        break
-plt.xlabel("time index")
-axs[0].set_title("shifts (in pixels)")
-axs[0].set_ylabel("vertical")
-axs[1].set_ylabel("horizontal")
-plt.savefig(os.path.join(saveDir, "shifts.png"))
-
-if n_rebin>1:
-    shifts = cmovie(shifts.reshape((1,)+shifts.shape)).resize(n_rebin,1,1)[0]
-
-m_rshifted = np.zeros_like(orig_movie[:len(shifts)])
-
-di = len(shifts)//20
-
-mtype = metadata["bit depth"]
-maxv = 2**int(str(mtype).strip("uint"))-1
-
-for i in range(0,len(shifts),di):
-    sl = slice(i,min(len(shifts),i+di))
-    print(sl.start, sl.stop)
-    tmpm = orig_movie[sl].apply_shifts(shifts[sl])
-    tmpm[tmpm<0] = 0
-    tmpm[tmpm>maxv] = maxv
-    tmpm = np.round(tmpm)
-    m_rshifted[sl] = tmpm
-
-del orig_movie, rec
 
 if len(rec.metadata)==1:
     movieFilename = os.path.join(saveDir, ".".join(rec.Experiment.split(".")[:-1]+["mp4"]))
 else:
     movieFilename = os.path.join(saveDir, rec.Experiment+"_"+ser+".mp4")
 
-movieFilename = movieFilename.replace(".mp4", "_rigshifted.tif")
+outMovieFilename =  movieFilename.replace(".mp4", "_rigshifted.mp4")
+outTifFilename = outMovieFilename.replace(".mp4", ".tif")
 
-im = PIL.Image.fromarray(m_rshifted[0])
-
-im.save(movieFilename,
-        save_all=True,
-        append_images=[PIL.Image.fromarray(m_rshifted[i]) for i in range(1,len(m_rshifted))],
-        compression="tiff_deflate"
-       )
-
-saveMovie(m_rshifted, movieFilename.replace(".tif",".mp4"), )
+orig_movie = cmovie(rec.Series[ser]["data"][:-1], fr=freq)
 bf.javabridge.kill_vm()
+
+if args.debug:
+    assert False ########################## debug stop ###########
+
+m_rshifted = tifmemmap(
+    outTifFilename,
+    shape=orig_movie.shape,
+    dtype=orig_movie.dtype,
+    photometric="minisblack"
+)
+cell_half_width_in_px = int(np.ceil(6 / metadata.pxSize))
+max_dev_rig = cell_half_width_in_px // 2 + 1
+
+if args.correct_phase:
+    ####################
+    even_template = orig_movie[:int(orig_movie.fr)+1,::2].mean(0)
+    gentle_motion_correct(
+        orig_movie[:,::2],
+        m_rshifted[:,::2],
+        plot_name=movieFilename.split(".mp4")[0]+"_even_shifts.png",
+        template=even_template,
+        max_dev_rig = (max_dev_rig, max_dev_rig)
+    )
+    odd_template = odd_template = (even_template[:-1]+even_template[1:])/2
+    gentle_motion_correct(
+        orig_movie[:,1::2],
+        m_rshifted[:,1::2],
+        plot_name=movieFilename.split(".mp4")[0]+"_odd_shifts.png",
+        template=odd_template,
+        max_dev_rig,
+        max_dev_rig = (max_dev_rig, max_dev_rig)
+    )
+else:
+    gentle_motion_correct(
+        orig_movie,
+        m_rshifted,
+        plot_name=movieFilename.split(".mp4")[0]+"_shifts.png",
+        max_dev_rig = (max_dev_rig, max_dev_rig)
+    )
+
+m_rshifted.flush()
+
+del orig_movie, rec
+
+saveMovie(m_rshifted, outMovieFilename)
+
 quit()
