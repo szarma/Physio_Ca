179ecc939b88e7bd8f699d643595ce805ec2e7d1
Hannsen <1245029@students.vetmeduni.ac.at>
2021-12-20 19:02:40 +0100

    Latest approach of adding database functionality to islets as a separate module.

--- islets/database/ExperimentDatabase.py
+++ islets/database/ExperimentDatabase.py
@@ -10,6 +10,11 @@ import sqlite3
 from typing import List, NamedTuple, Optional, Tuple, Union, Pattern
 from sys import stdout
 
+import islets.Recording
+from . import SUPPORTED_FORMATS
+from ..Recording import Recording, parse_leica
+from ..utils import get_series_dir
+
 class ExperimentDatabase:
     _logger: logging.Logger
     connection: sqlite3.Connection
@@ -19,6 +24,11 @@ class ExperimentDatabase:
         """Standard constructor of a database manager for CTN experiments/recordings.
 
         Constructor of an instance of the ExperimentDatabase. The database uses sqlite as an engine.
+
+        Raises
+        ------
+        IOError
+            Gets raised if the database cannot be opened due to failures during IO.
 
         Parameters
         ----------
@@ -69,3 +79,58 @@ class ExperimentDatabase:
         self._cursor.close()
         self.connection.close()
         self._logger.info('Connection closed.')
+
+    def __get_recording(self, root_dir: Path) -> List[Path]:
+        """Function getting all recordings.
+
+        Parameters
+        ----------
+        root_dir : Path
+            Directory, which is considered as root to search for experiment files.
+
+        Returns
+        -------
+        List[Path]
+            List of paths to all found recordings.
+        """
+        file_list: List[Path] = []
+        for path_object in root_dir.rglob('*'):
+            try:
+                if path_object.suffix in SUPPORTED_FORMATS:
+                    self._logger.debug(f'Found file while indexing: "{path_object.as_posix()}"')
+                    file_list.append(path_object)
+            except PermissionError as e:
+                self._logger.error(f'Permission denied accessing file: "{e.filename}"')
+
+        return file_list
+
+    def refresh(self, root_dir: Path, wipe: bool = False) -> None:
+        """Refresh the database.
+
+        Function, which can be called to refresh the database. It can either be rewritten by wiping all previous data or
+        if can just add newly found entries to the existing database.
+        Experiments will be searched in each subfolder of the root directory.
+
+        Parameters
+        ----------
+        root_dir : Path
+            Path of the directory, which will be used as root directory to refresh the database.
+        wipe : bool, default: False
+            Indicator whether the database should be rewritten completely. Default: False
+
+        Returns
+        -------
+        None
+        """
+        recordings: List[Path]
+
+        recordings = self.__get_recording(root_dir=root_dir)
+        for recording in recordings:
+            rec_data = Recording(recording.as_posix())
+            analysis_folder = recording.parent / (recording.name + '_analysis')
+            if not analysis_folder.is_dir():
+                self._logger.debug(f'No analysis folder could be found for "{recording.as_posix()}".')
+            else:
+                pass
+
+        raise NotImplementedError
