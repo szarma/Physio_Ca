4f16717a01df17193dfd26265a770a6280ebeaf5
Johannes Pfabe <johannes.pfabe@meduniwien.ac.at>
2023-03-08 15:16:43 +0100

    Updated initial docker configuration

--- docker/jupyterhub/jupyterhub_config.py
+++ docker/jupyterhub/jupyterhub_config.py
@@ -1,35 +1,40 @@
 import os
 import sys
 
-from cdsdashboards.app import CDS_TEMPLATE_PATHS
-from cdsdashboards.hubextension import cds_extra_handlers
+import dockerspawner
 
 c = get_config()
+
+
+class CustomDockerSpawner(dockerspawner.DockerSpawner):
+    def create_object(self):
+        self.extra_create_kwargs['ports'] = {
+            "%i/tcp" % self.port: None,
+            "%i/tcp" % 8050: None
+        }
+        return super().create_object()
+
 
 c.JupyterHub.log_level = 'DEBUG'
 c.JupyterHub.admin_access = True
 c.JupyterHub.hub_ip = '0.0.0.0'
-c.JupyterHub.allow_named_servers = True
-
-c.JupyterHub.template_paths = CDS_TEMPLATE_PATHS
-c.JupyterHub.extra_handlers = cds_extra_handlers
-c.CDSDashboardsConfig.builder_class = 'cdsdashboards.builder.dockerbuilder.DockerBuilder'
-c.CDSDashboardsConfig.conda_envs = ['']
-c.CDSDashboardsConfig.presentation_types = ['plotlydash']
+c.JupyterHub.hub_connect_ip = os.environ.get('HUB_IP') or 'jupyterhub'
+c.JupyterHub.allow_named_servers = False
+c.JupyterHub.spawner_class = CustomDockerSpawner
 
 c.Authenticator.admin_users = {'admin'}
 c.LocalAuthenticator.create_system_users = True
 
-# c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.JupyterHub.spawner_class = 'cdsdashboards.hubextension.spawners.variabledocker.VariableDockerSpawner'
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.environment = {'URL': os.getenv('HOST', default='ctn2.physiologie.meduniwien.ac.at')}
 c.DockerSpawner.volumes = {'jupyterhub-user-{username}': notebook_dir}
-c.DockerSpawner.name_template = "{prefix}-{username}-{servername}"
+c.DockerSpawner.name_template = "{prefix}-{username}"
 c.DockerSpawner.debug = True
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER'] + ':' + os.environ['ISLETS_VERSION']
 c.DockerSpawner.remove = True
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.DockerSpawner.use_internal_ip = True
 
 c.JupyterHub.services = [
     {
